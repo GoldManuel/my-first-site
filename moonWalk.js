a2d5aed2c4859d62a373790b9891a001ec735327
GoldManuel <goldmanly970@gmail.com>
2024-04-17 11:47:59 +0100

    My first commit

---
+++ moonWalk.js
@@ -0,0 +1,31 @@
+let col1 = document.getElementById('circleBase col1');
+let col2 = document.getElementById('circleBase col2');
+let col3 = document.getElementById('circleBase col3'); 
+    
+   function one() {
+    col1.style.backgroundColor ='red';
+    col2.style.background ='none';
+    col3.style.background ='none';
+   } 
+   function two() {
+    col1.style.background = 'none';
+    col2.style.backgroundColor = 'yellow';
+    col3.style.background = 'none';
+   } 
+
+   function three() {
+    col1.style.background ='none';
+    col2.style.background ='none';
+    col3.style.backgroundColor ='green';
+   } 
+
+   //  col1.addEventListener('click', one);
+   //  col2.addEventlistener('click', two);
+   //  col3.addEventlistener('click', three);
+    
+    
+    
+    
+    
+    
+    
